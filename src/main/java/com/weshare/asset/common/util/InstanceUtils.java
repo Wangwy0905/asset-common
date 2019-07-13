a1b7a677ec18451daea17d0e0c247296d080d1c9
zhibin.wang <zhibin.wang@weshareholdings.com>
2019-07-14 04:32:35 +0800

    增加默认构造函数，保证Response可以被反序列化

---
+++ src/main/java/com/weshare/asset/common/util/InstanceUtils.java
@@ -0,0 +1,56 @@
+package com.weshare.asset.common.util;
+
+import org.springframework.lang.Nullable;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+
+/**
+ * @Author: CY
+ * @Date: 2019/7/9 21:07
+ * @Version: 1.0
+ */
+public class InstanceUtils {
+    @Nullable
+    public static String getLocalHost() {
+        return getLocalHostLANAddress().getHostAddress();
+    }
+
+    @Nullable
+    public static InetAddress getLocalHostLANAddress() {
+        try {
+            InetAddress candidateAddress = null;
+            // 遍历所有的网络接口
+            for (Enumeration ifaces = NetworkInterface.getNetworkInterfaces(); ifaces.hasMoreElements();) {
+                NetworkInterface iface = (NetworkInterface) ifaces.nextElement();
+                // 在所有的接口下再遍历IP
+                for (Enumeration inetAddrs = iface.getInetAddresses(); inetAddrs.hasMoreElements();) {
+                    InetAddress inetAddr = (InetAddress) inetAddrs.nextElement();
+                    if (!inetAddr.isLoopbackAddress()) {// 排除loopback类型地址
+                        if (inetAddr.isSiteLocalAddress()) {
+                            // 如果是site-local地址，就是它了
+                            return inetAddr;
+                        } else if (candidateAddress == null) {
+                            // site-local类型的地址未被发现，先记录候选地址
+                            candidateAddress = inetAddr;
+                        }
+                    }
+                }
+            }
+            if (candidateAddress != null) {
+                return candidateAddress;
+            }
+
+            InetAddress jdkSuppliedAddress = InetAddress.getLocalHost();
+            if (jdkSuppliedAddress == null) {
+                throw new UnknownHostException("The JDK InetAddress.getLocalHost() method unexpectedly returned null.");
+            }
+            return jdkSuppliedAddress;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
