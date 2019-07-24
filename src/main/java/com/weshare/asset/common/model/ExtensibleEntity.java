c53eb429415bcbf8ecec99ea115b732c47fd70c3
zhibin.wang <zhibin.wang@weshareholdings.com>
2019-07-24 14:30:57 +0800

    修改ExtensibleEntity对象定义

--- src/main/java/com/weshare/asset/common/model/ExtensibleEntity.java
+++ src/main/java/com/weshare/asset/common/model/ExtensibleEntity.java
@@ -11,33 +11,44 @@ import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 基础扩展类，被用于接受网络请求中的入参，保存所有未知参数，但不提供相关属性的getter方法
+ * @author zhibin.wang
+ */
 @Data
 public class ExtensibleEntity {
     @JsonIgnore
     Map<String, Object> extension = new HashMap<String, Object>();
 
+    /**
+     * 该方法设计成private，保证extension属性只能在内部使用，不能通过get方法获取到
+     * 该方法仅作为和Jackson对接的接口，在其他任何场合都不再使用
+     * @return
+     */
     @JsonAnyGetter
-    public Map<String, Object> get() {
+    private Map<String, Object> get() {
         return extension;
     }
 
+    /**
+     * 该方法设计成private，保证extension属性只能在内部使用，不能通过get方法获取到
+     * 该方法仅作为和Jackson对接的接口，在其他任何场合都不再使用
+     * @return
+     */
     @JsonAnySetter
-    public void set(String key, Object value) {
-        if (setPrimitiveProperty(key, value)) return ;
+    private void set(String key, Object value) {
+        if (setPrimitiveProperty(key, value)) {
+            return ;
+        }
+
         extension.put(key, value);
-    }
-
-    public <T> T get(String nodeCode, Class<T> type){
-        Object result = getPrimitiveProperty(nodeCode);
-        if (result == null)
-            result = getExtendProperty(nodeCode);
-
-        return ConversionUtils.convert(result, type);
     }
 
     private boolean setPrimitiveProperty(String key, Object value) {
         Field field = ReflectionUtils.findField(this.getClass(), key);
-        if (field == null) return false;
+        if (field == null) {
+            return false;
+        }
 
         field.setAccessible(true);
         try {
@@ -49,18 +60,27 @@ public class ExtensibleEntity {
         return true;
     }
 
-    private Object getPrimitiveProperty(String nodeCode) {
-        try {
-            Field field = ReflectionUtils.findField(this.getClass(), nodeCode);
-            field.setAccessible(true);
+//    public <T> T get(String nodeCode, Class<T> type){
+//        Object result = getPrimitiveProperty(nodeCode);
+//        if (result == null) {
+//            result = getExtendProperty(nodeCode);
+//        }
+//
+//        return ConversionUtils.convert(result, type);
+//    }
 
-            return ReflectionUtils.getField(field, this);
-        } catch (Throwable ex) {
-            return null;
-        }
-    }
+//    private Object getPrimitiveProperty(String nodeCode) {
+//        try {
+//            Field field = ReflectionUtils.findField(this.getClass(), nodeCode);
+//            field.setAccessible(true);
+//
+//            return ReflectionUtils.getField(field, this);
+//        } catch (Throwable ex) {
+//            return null;
+//        }
+//    }
 
-    private Object getExtendProperty(String nodeCode) {
-        return extension.get(nodeCode);
-    }
+//    private Object getExtendProperty(String nodeCode) {
+//        return extension.get(nodeCode);
+//    }
 }
