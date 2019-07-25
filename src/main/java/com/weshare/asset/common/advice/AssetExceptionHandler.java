5c0f9b6872c2a13b69a5e4124ec77b9eba3fff6d
zhibin.wang <zhibin.wang@weshareholdings.com>
2019-07-25 11:27:36 +0800

    修改异常处理框架

--- src/main/java/com/weshare/asset/common/advice/AssetExceptionHandler.java
+++ src/main/java/com/weshare/asset/common/advice/AssetExceptionHandler.java
@@ -3,6 +3,7 @@ package com.weshare.asset.common.advice;
 import com.weshare.asset.common.exception.AssetException;
 import com.weshare.asset.common.model.Response;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,10 +13,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  */
 @RestControllerAdvice
 public class AssetExceptionHandler {
-    @ExceptionHandler({HttpMessageNotReadableException.class})
-    public Response bindExceptionHandler(HttpMessageNotReadableException ex) {
-        return new Response(Response.BUSINESS_ERROR, ex.getMessage());
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public Response methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException ex) {
+        // 422 表示无法处理的实体
+        return new Response(422, ex.getMessage());
     }
+
 
     @ExceptionHandler({AssetException.class})
     public Response assetExceptionHandler(AssetException ex) {
