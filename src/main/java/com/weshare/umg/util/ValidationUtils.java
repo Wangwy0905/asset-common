ece127a7bb123fd836532bbc189e4cfc450f4d7f
zhibin.wang <zhibin.wang@weshareholdings.com>
2019-08-05 20:20:28 +0800

    2.0.0-SNAPSHOT 调整umg对fastjson的依赖

---
+++ src/main/java/com/weshare/umg/util/ValidationUtils.java
@@ -0,0 +1,54 @@
+package com.weshare.umg.util;
+
+import com.weshare.umg.exception.UmgApiException;
+import com.weshare.umg.remote.UmgConfig;
+import com.weshare.umg.response.base.Result;
+import com.weshare.umg.system.ClientType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ValidationUtils {
+    private static final Logger logger = LoggerFactory.getLogger(ValidationUtils.class);
+
+    public ValidationUtils() {
+    }
+
+    public static void validateUmConfig(UmgConfig config) {
+        if (config == null) {
+            throw new IllegalArgumentException("UmgConfig Cannot Be null");
+        } else {
+            String appCode = config.getAppCode();
+            if (appCode == null || appCode.trim().length() == 0) {
+                throw new IllegalArgumentException("app code Cannot Be null");
+            } else {
+                String token = config.getToken();
+                if (token == null || token.trim().length() == 0) {
+                    throw new IllegalArgumentException("token code Cannot Be null");
+                } else {
+                    Integer clientType = config.getClientType();
+                    if (clientType == null) {
+                        throw new IllegalArgumentException("client type Cannot Be null");
+                    } else {
+                        ClientType type = ClientType.getByValue(clientType);
+                        if (type == null) {
+                            throw new IllegalArgumentException("client type error");
+                        } else {
+                            String urls = config.getUmgurl();
+                            if (type.equals(ClientType.HTTP) && (urls == null || urls.trim().length() == 0)) {
+                                throw new IllegalArgumentException("umg urls Cannot Be null");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static void validationResult(String body) throws UmgApiException {
+
+    }
+
+    public static void validationResult(Result result) throws UmgApiException {
+
+    }
+}
