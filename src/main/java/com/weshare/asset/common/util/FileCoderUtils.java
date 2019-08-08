829a577f107aa704ca4728ca8a68dd469ce0a8d4
zhibin.wang <zhibin.wang@weshareholdings.com>
2019-08-08 22:14:54 +0800

    2.0.0-SNAPSHOT

--- src/main/java/com/weshare/asset/common/util/FileCoderUtils.java
+++ src/main/java/com/weshare/asset/common/util/FileCoderUtils.java
@@ -1,7 +1,9 @@
 package com.weshare.asset.common.util;
 
+import com.weshare.asset.common.exception.AssetException;
 import com.weshare.asset.common.exception.BizException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.util.Assert;
 import org.springframework.util.Base64Utils;
 import sun.misc.BASE64Decoder;
@@ -25,6 +27,19 @@ public class FileCoderUtils {
             return new ByteArrayInputStream(bytes);
         } catch (IOException e) {
             throw new BizException(500005, "base64格式文件转inputStream异常", e);
+        }
+    }
+
+    public static String createBase64(InputStream in) throws BizException {
+        byte[] buff = new byte[100];
+        int rc = 0;
+        try (ByteArrayOutputStream swapStream = new ByteArrayOutputStream()) {
+            while ((rc = in.read(buff, 0, 100)) > 0) {
+                swapStream.write(buff, 0, rc);
+            }
+            return new String(Base64.encodeBase64(swapStream.toByteArray()));
+        } catch (IOException e) {
+            throw new BizException(500006, "inputStream格式文件转base64异常", e);
         }
     }
 
