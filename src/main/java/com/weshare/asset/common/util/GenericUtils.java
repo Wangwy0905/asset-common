ef28ae1d58d736f87fdd5f634df44016709c6c64
zhibin.wang <zhibin.wang@weshareholdings.com>
2019-08-15 10:41:07 +0800

    2.0.0-SNAPSHOT cmq框架调整

--- src/main/java/com/weshare/asset/common/util/GenericUtils.java
+++ src/main/java/com/weshare/asset/common/util/GenericUtils.java
@@ -1,29 +1,59 @@
 package com.weshare.asset.common.util;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.Nullable;
 import org.springframework.util.Assert;
 import org.springframework.util.ReflectionUtils;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+@Slf4j
 public class GenericUtils {
-    public static Class<?> getGenericType(Object object) {
+    @Nullable public static Class<?> getGenericType(Object object) {
         Assert.notNull(object, "传入Class对象不能为空");
-        Type type = object.getClass().getGenericSuperclass();
+        List<Type> genericSuperList = getAllGenericSuper(object);
+        for (Type type : genericSuperList) {
+            Class<?> result = getGenericType(type);
+            if (result != null) {
+                return result;
+            }
+        }
 
+        return null;
+    }
+
+    private static List<Type> getAllGenericSuper(Object object) {
+        List<Type> result = new ArrayList<>();
+        Type superClass = object.getClass().getGenericSuperclass();
+        if (superClass != null) {
+            result.add(superClass);
+        }
+
+        Type[] interfaces = object.getClass().getGenericInterfaces();
+        if (interfaces != null && interfaces.length > 0) {
+            result.addAll(Arrays.asList(interfaces));
+        }
+        return result;
+    }
+
+    private static Class<?> getGenericType(Type type) {
         if (type == null) {
-            throw new IllegalArgumentException("传入参数异常，无法获取其泛型类型的父类对象");
+            return null;
         }
 
         Field field = ReflectionUtils.findField(type.getClass(),"actualTypeArguments");
         if (field == null) {
-            throw new IllegalArgumentException("传入参数异常，无法获取其泛型类型的父类对象");
+            return null;
         }
 
         field.setAccessible(true);
         Type[] types = (Type[])ReflectionUtils.getField(field, type);
         if (types == null || types.length == 0) {
-            throw new IllegalArgumentException("传入参数异常，无法获取其泛型类型的父类对象");
+            return null;
         }
 
         return (Class)types[0];
