2f4e5e88116929efef36a6a7bf592f7129aaf08b
zhibin.wang <zhibin.wang@weshareholdings.net>
2019-08-20 19:53:20 +0800

    2.0.0-SNAPSHOT 风控模型调用框架

--- src/main/java/com/weshare/asset/common/util/GenericUtils.java
+++ src/main/java/com/weshare/asset/common/util/GenericUtils.java
@@ -14,26 +14,43 @@ import java.util.List;
 @Slf4j
 public class GenericUtils {
     @Nullable public static Class<?> getGenericType(Object object) {
-        Assert.notNull(object, "传入Class对象不能为空");
-        List<Type> genericSuperList = getAllGenericSuper(object);
-        for (Type type : genericSuperList) {
+        Assert.notNull(object, "传入Object对象不能为空");
+        return getGenericType(object.getClass());
+    }
+
+    public static Class<?> getGenericType(Class clazz) {
+        Assert.notNull(clazz, "传入Class对象不能为空");
+        List<Type> genericSuperList = getAllGenericSuper(clazz);
+        return getGenericType(genericSuperList);
+    }
+
+    public static Class<?> getGenericType(List<Type> typeList) {
+        for (Type type : typeList) {
             Class<?> result = getGenericType(type);
             if (result != null) {
                 return result;
             }
-        }
 
+            if (!(type instanceof Class)) {
+                continue;
+            }
+
+            result = getGenericType(getAllGenericSuper((Class)type));
+            if (result != null) {
+                return result;
+            }
+        }
         return null;
     }
 
-    private static List<Type> getAllGenericSuper(Object object) {
+    private static List<Type> getAllGenericSuper(Class clazz) {
         List<Type> result = new ArrayList<>();
-        Type superClass = object.getClass().getGenericSuperclass();
+        Type superClass = clazz.getGenericSuperclass();
         if (superClass != null) {
             result.add(superClass);
         }
 
-        Type[] interfaces = object.getClass().getGenericInterfaces();
+        Type[] interfaces = clazz.getGenericInterfaces();
         if (interfaces != null && interfaces.length > 0) {
             result.addAll(Arrays.asList(interfaces));
         }
