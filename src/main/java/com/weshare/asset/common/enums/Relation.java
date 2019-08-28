c91f592a9aea4d44c94f76a2f879aac95423c87c
v_chenyang <v_chenyang@weshareholdings.com>
2019-08-28 17:16:38 +0800

    feat：增加 联系人关系枚举

--- src/main/java/com/weshare/asset/common/enums/Relation.java
+++ src/main/java/com/weshare/asset/common/enums/Relation.java
@@ -6,18 +6,29 @@ public enum Relation {
     COLLEAGUE("同事"),
     PARENT("父母"),
     CHILD("子女"),
-    BROTHER("兄弟姐妹"),
     RELATIVE("亲戚"),
     CLERK("店员"),
     LANDLORD("房东"),
-    ;
+    BROTHER("兄弟"),
+    CLASSMATE("同学"),
+    OTHERRELATIVE("其他亲属"),
+    OTHER("其他"),
+    BROTHERANDSISTER("兄妹"),
+    SIBLING("姐弟"),
+    SISTER("姐妹"),
+    ONESELF("本人");
+
 
     Relation(String name) {
         this.name = name;
     }
 
     private String name;
+
     public String getName() {
         return this.name;
     }
+    // 兄弟、同学、其他亲属、其他、兄妹、接地、姐妹、本人
+
+
 }
