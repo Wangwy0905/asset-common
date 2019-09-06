720e0a24d871c6ff99c9e808279ef8232d597e7d
zhibin.wang <zhibin.wang@weshareholdings.net>
2019-09-06 16:49:50 +0800

    2.0.0-SNAPSHOT 衍生变量

--- src/main/java/com/weshare/asset/common/enums/ApplyPhase.java
+++ src/main/java/com/weshare/asset/common/enums/ApplyPhase.java
@@ -1,25 +1,31 @@
 package com.weshare.asset.common.enums;
 
 public enum ApplyPhase {
-    SA("/process/create"),
-    AUTH("/process/auth"),
-    FACE("/process/face"),
-    PBOC("/process/pboc"),
-    BI("/process/basic-info"),
-    AC("/process/add-contact"),
-    PC("/process/policy-car"),
-    APV("/process/apv"),
-    BC("/process/bank-card"),
-    ML("/process/make-loan"),
-    FIN("/process/finish")
+    SA("/process/create", ""),
+    AUTH("/process/auth", ""),
+    FACE("/process/face", ""),
+    PBOC("/process/pboc", ""),
+    BI("/process/basic-info", ""),
+    AC("/process/add-contact", ""),
+    PC("/process/policy-car", ""),
+    APV("/process/apv", ""),
+    BC("/process/bank-card", ""),
+    ML("/process/make-loan", ""),
+    FIN("/process/finish", "")
     ;
 
     private String serviceName;
-    ApplyPhase(String serviceName) {
+    private String phaseName;
+    ApplyPhase(String serviceName, String phaseName) {
         this.serviceName = serviceName;
+        this.phaseName = phaseName;
     }
 
     public String getServiceName() {
         return this.serviceName;
+    }
+
+    public String getPhaseName() {
+        return this.phaseName;
     }
 }
