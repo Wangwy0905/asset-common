31d72860c5fd58f6b06a58eabbd8240f2e133ce3
yuchen.hu <3edc#EDC>
2019-09-09 17:37:02 +0800

    加入@CreateBy和@LastModifyBy

--- src/main/java/com/weshare/asset/common/entity/EntityBase.java
+++ src/main/java/com/weshare/asset/common/entity/EntityBase.java
@@ -3,6 +3,8 @@ package com.weshare.asset.common.entity;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -14,9 +16,11 @@ public class EntityBase {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
 
-    @Column(name="CREATOR", length=128, nullable=false, unique=false)
+    @Column(name = "CREATOR", length = 128, nullable = false, unique = false)
+    @CreatedBy
     protected String creator = "system";
-    @Column(name="MODIFIER", length=128, nullable=false, unique=false)
+    @Column(name = "MODIFIER", length = 128, nullable = false, unique = false)
+    @LastModifiedBy
     protected String modifier = "system";
 
     @CreationTimestamp
