11b53546161dac0ed9286e5d7b1a25b427008567
v_wangwenyi <v_wangwenyi@weshareholdings.com>
2019-09-16 18:37:21 +0800

    fix： DateUtils日期添加addDate  优化

--- src/main/java/com/weshare/asset/common/util/DateUtils.java
+++ src/main/java/com/weshare/asset/common/util/DateUtils.java
@@ -110,10 +110,11 @@ public class DateUtils {
         return Date.from(date.atZone(ZoneId.systemDefault()).toInstant());
     }
 
-    public static Date addDate(Date date, long day) {
-        long time = date.getTime(); // 得到指定日期的毫秒数
-        day = day * 24 * 60 * 60 * 1000; // 要加上的天数转换成毫秒数
-        time += day; // 相加得到新的毫秒数
-        return new Date(time); // 将毫秒数转换成日期
+    public static Date addDate(Date date, int day) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DATE,day);
+        Date time = calendar.getTime();
+        return time;
     }
 }
