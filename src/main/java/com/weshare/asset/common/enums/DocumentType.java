08751dae04d2ac080b055066aadb991bf5eb890d
v_songfeng <v_songfeng@weshareholdings.com>
2019-09-25 10:07:40 +0800

    合同签章新增件文件类型

--- src/main/java/com/weshare/asset/common/enums/DocumentType.java
+++ src/main/java/com/weshare/asset/common/enums/DocumentType.java
@@ -5,14 +5,13 @@ package com.weshare.asset.common.enums;
  */
 public enum DocumentType {
     APP_REGISTRY("用户注册协议", false),
-    PRIVACY_POLICY("分享贷隐私政策", false),
-    PERSONAL_INFO_USE_GRANT("个人信息使用授权协议", false),
-    PBOC_QUERY_GRANT("个人征信查询授权协议", true),
-    PERSONAL_INFO_QUERY_GRANT("个人信息查询授权协议", true),
-    LOAN_CREDIT_GRANT("信用贷款授信协议", true),
-    LOAN_CONTRACT("借款合同", true),
+    PRIVACY_POLICY("隐私政策", false),
+    COMPREHENSIVE_AUTHORIZATION("综合授权书", false),
+    PBOC_QUERY_GRANT("人行授权协议", true),
     ELECTRONIC_SIGNATURE_GRANT("电子签名授权书", true),
-    ENTRUSTED_WITHHOLDING("委托代扣协议", true),
+    LOAN_CONTRACT_12("贷款合同", true),
+    LOAN_CONTRACT_36("贷款合同", true),
+    ENTRUSTED_WITHHOLDING("扣款协议书", true),
     ID_CARD_FRONT("身份证正面照片", false),
     ID_CARD_BACK("身份证反面照片", false),
     FACE_DETECT_IMAGE("人脸识别照片", false);
