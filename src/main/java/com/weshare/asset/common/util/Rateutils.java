7a17f5ec0d41c45da3ff8b3e39bdf77eb29687a9
v_zhouziqi <v_zhouziqi@weshareholdings.com>
2019-10-09 14:15:05 +0800

    增加了凤金透传接口

--- src/main/java/com/weshare/asset/common/util/Rateutils.java
+++ src/main/java/com/weshare/asset/common/util/Rateutils.java
@@ -1,20 +1,32 @@
 package com.weshare.asset.common.util;
 
-import org.springframework.util.Assert;
-
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.text.NumberFormat;
 
 public class Rateutils {
 
     public static Double convertToDouble(String rate) {
-        Assert.notNull(rate, "费率不能为空");
+        if (rate == null) {
+            return null;
+        }
+
         try {
             NumberFormat nf = NumberFormat.getPercentInstance();
             Number number = nf.parse(rate);
             return number.doubleValue();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String convertToString(BigDecimal rate) {
+        if (rate == null) {
             return null;
         }
+
+        DecimalFormat df = new DecimalFormat("0.00%");
+        return df.format(rate);
     }
 }
