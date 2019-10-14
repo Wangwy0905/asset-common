d8a0ee4938c3c1b13769ae8de7c9a70b25941400
v_songfeng <v_songfeng@weshareholdings.com>
2019-10-14 18:07:53 +0800

    车险类型

--- src/main/java/com/weshare/asset/common/enums/PolicyCarType.java
+++ src/main/java/com/weshare/asset/common/enums/PolicyCarType.java
@@ -1,18 +1,7 @@
 package com.weshare.asset.common.enums;
 
 public enum PolicyCarType {
-
-    SMALL_CAR_PLATE("02"),
-    SMALL_NEW_ENERGY_CAR("52"),
-   ;
-
-    private String policyCarTypeName;
-
-    private PolicyCarType(String policyCarTypeName) {
-        this.policyCarTypeName = policyCarTypeName;
-    }
-
-    public String getPolicyCarTypeName() {
-        return this.policyCarTypeName;
-    }
+    SMALL_CAR_PLATE,
+    SMALL_NEW_ENERGY_CAR,
+;
 }
