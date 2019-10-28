0e570885a90f3eb004efa19c23d45044f99e5804
v_zhaohuadan <v_zhaohuadan@weshareholdings.com>
2019-10-28 18:34:58 +0800

    feat:婚姻状态枚举修改

--- src/main/java/com/weshare/asset/common/enums/MaritalState.java
+++ src/main/java/com/weshare/asset/common/enums/MaritalState.java
@@ -1,38 +1,31 @@
 package com.weshare.asset.common.enums;
 
 public enum MaritalState {
-    WEIHUN(10, "未婚"),
-    YIHUN(20, "已婚"),
-    CHUHUN(21, "初婚"),
-    ZAIHUN(22, "再婚"),
-    FUHUN(23, "复婚"),
-    SANGOU(30, "丧偶"),
-    LIHUN(40, "离婚"),
-    UNKNOWN(90, "未说明的婚姻状况"),
+    WEIHUN("未婚"),
+    YIHUN("已婚"),
+    CHUHUN("初婚"),
+    ZAIHUN("再婚"),
+    FUHUN("复婚"),
+    SANGOU("丧偶"),
+    LIHUN("离婚"),
+    UNKNOWN("未说明的婚姻状况"),
     ;
 
-    MaritalState(int code, String name) {
-        this.code = code;
+    MaritalState(String name) {
         this.name = name;
     }
 
-    private int code;
-
-    private String name;
-
-    public int getCode() {
-        return this.code;
-    }
-    public String getName() {
-        return this.name;
-    }
-
-    public static MaritalState getMarital(int code) {
+    public static MaritalState getMarital(String maritalStateName) {
         for (MaritalState maritalState : values()) {
-            if (maritalState.code == code) {
+            if (maritalState.name.equals(maritalStateName)) {
                 return maritalState;
             }
         }
         return null;
+    }
+
+    private String name;
+    public String getName() {
+        return this.name;
     }
 }
