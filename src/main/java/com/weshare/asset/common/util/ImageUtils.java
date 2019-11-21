81f7172b705501f888d98532c1f7434e7e5717b2
yuchen.hu <1qaz!QAZ>
2019-11-21 17:53:51 +0800

    refactor

--- src/main/java/com/weshare/asset/common/util/ImageUtils.java
+++ src/main/java/com/weshare/asset/common/util/ImageUtils.java
@@ -1,6 +1,8 @@
 package com.weshare.asset.common.util;
 
+import com.weshare.asset.common.exception.AssetException;
 import com.weshare.asset.common.exception.ServiceException;
+import org.springframework.util.Assert;
 import sun.misc.BASE64Decoder;
 
 import javax.imageio.ImageIO;
@@ -12,32 +14,34 @@ public class ImageUtils {
 
     /**
      * 校验base64是否为BMP、GIF、JPG、PNG格式
+     *
      * @param imgBase64Str base64字符串
      * @return true: 格式正确
      */
-    public static boolean isImage(String imgBase64Str) throws ServiceException {
+    public static boolean isImage(String imgBase64Str) throws AssetException {
         if (imgBase64Str == null || imgBase64Str.length() == 0) {
             return false;
-        } else {
-
-            BASE64Decoder decoder = new BASE64Decoder();
-            byte[] byteArray = new byte[0];
-            try {
-                byteArray = decoder.decodeBuffer(imgBase64Str);
-            } catch (IOException e) {
-               throw new ServiceException("base64解码失败", e);
-            }
-
-            try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArray)) {
-                BufferedImage bufImg = ImageIO.read(byteArrayInputStream);
-                if (bufImg == null) {
-                    return false;
-                }
-                bufImg = null;
-            } catch (IOException e) {
-                throw new ServiceException("base64转图片异常", e);
-            }
         }
-        return true;
+
+        try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(decodeBuffer(imgBase64Str))) {
+            BufferedImage bufImg = ImageIO.read(byteArrayInputStream);
+            if (bufImg == null) {
+                return false;
+            }
+            return true;
+        } catch (IOException e) {
+            throw new ServiceException("base64转图片异常", e);
+        }
+
+    }
+
+    private static byte[] decodeBuffer(String imgBase64Str) throws AssetException {
+        Assert.isTrue(imgBase64Str != null && imgBase64Str.length() != 0, "传入的字符串不为null或空串");
+        BASE64Decoder decoder = new BASE64Decoder();
+        try {
+            return decoder.decodeBuffer(imgBase64Str);
+        } catch (IOException e) {
+            throw new ServiceException("base64解码失败", e);
+        }
     }
 }
