e0c9c6bed8c783b76843229fad4060e7c16bddfa
v_zhouziqi <v_zhouziqi@weshareholdings.com>
2019-11-21 16:54:55 +0800

    feat:增加base64图片格式校验工具

---
+++ src/main/java/com/weshare/asset/common/util/ImageUtils.java
@@ -0,0 +1,46 @@
+package com.weshare.asset.common.util;
+
+import com.weshare.asset.common.exception.ServiceException;
+import sun.misc.BASE64Decoder;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+public class ImageUtils {
+
+    /**
+     * 校验base64是否为BMP、GIF、JPG、PNG格式
+     * @param imgBase64Str base64字符串
+     * @return true: 格式正确
+     */
+    public static boolean isImage(String imgBase64Str) throws ServiceException {
+        if (imgBase64Str == null || imgBase64Str.length() == 0) {
+            return false;
+        } else {
+            ByteArrayInputStream byteArrayInputStream = null;
+            try {
+                BASE64Decoder decoder = new BASE64Decoder();
+                byte[] byteArray = decoder.decodeBuffer(imgBase64Str);
+                byteArrayInputStream = new ByteArrayInputStream(byteArray);
+                BufferedImage bufImg = ImageIO.read(byteArrayInputStream);
+                if (bufImg == null) {
+                    return false;
+                }
+                bufImg = null;
+            } catch (IOException e) {
+                throw new ServiceException("base64转图片异常", e);
+            } finally {
+                if (byteArrayInputStream != null) {
+                    try {
+                        byteArrayInputStream.close();
+                    } catch (IOException e) {
+                        throw new ServiceException("base64转图片关闭流异常", e);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
